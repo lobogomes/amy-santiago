6b099bf0e55ce6fb1fd0738b90af10791b0c54cf
Amanda Lobo <8lobogomes@gmail.com>
2024-08-26 23:24:12 -0300

    first commit

---
+++ src/main/java/com/github/lobogomes/amysantiago/AmySantiagoApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lobogomes.amysantiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmySantiagoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmySantiagoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lobogomes/amysantiago/AmySantiagoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lobogomes.amysantiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmySantiagoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
