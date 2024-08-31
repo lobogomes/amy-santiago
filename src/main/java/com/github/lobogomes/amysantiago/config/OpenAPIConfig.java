18c3ad8653c15434721c46d2bced4fce873ba1c2
Amanda Lobo <8lobogomes@gmail.com>
2024-08-31 19:44:15 -0300

    #01 - Auth Configuration

---
+++ src/main/java/com/github/lobogomes/amysantiago/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.github.lobogomes.amysantiago.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "amy-santiago-api", version = "v1"),
+        security = @SecurityRequirement(name = "bearer token")
+)
+@SecurityScheme(
+        name = "bearer token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {
+
+}
